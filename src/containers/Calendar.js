fccbc1756595710c3c2870d70bf3630903dc762b
Edax Ucles <filanderuclez@gmail.com>
2020-11-11 15:45:40 -0600

    removes console.log

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -1,26 +1,28 @@
 import { useContext } from "react";
 import { Paper, Box } from "@material-ui/core";
 import styled from "styled-components";
-
 import { CalendarContext } from "../context";
-// import DateButton from "../components/DateButton";
 import CalendarHeader from "./Header";
 import WeekDays from "./WeekDays";
 import CellDays from "./CellDays";
-import { Container } from '../components/utils/commonComponents'
+import { Container } from "../components/utils/commonComponents";
+
 const Calendar = (props) => {
   const { state } = useContext(CalendarContext);
-  console.log("CurrentDate", state.currentDate);
 
   return (
-    <Paper elevation={3} style={{ margin: "5rem" }}>
+    <PaperContainer elevation={3}>
       <Container column>
         <CalendarHeader currentDate={state.currentDate} />
         <WeekDays />
         <CellDays />
       </Container>
-    </Paper>
+    </PaperContainer>
   );
 };
 
 export default Calendar;
+
+const PaperContainer = styled(Paper)`
+  margin: calc(100% / 17);
+`;
